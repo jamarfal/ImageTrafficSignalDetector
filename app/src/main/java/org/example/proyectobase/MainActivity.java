50a4a327a0a104cf672d58ca3115e88819dc578c
José Alberto Martín <jalbertomartinfalcon@gmail.com>
2017-04-20 22:03:41 +0200

    first commit

--- app/src/main/java/org/example/proyectobase/MainActivity.java
+++ app/src/main/java/org/example/proyectobase/MainActivity.java
@@ -233,6 +233,7 @@ public class MainActivity extends Activity implements CameraBridgeViewBase.CvCam
         }
 
         Mat salida = procesador.procesa(entrada);
+        procesador.mitadMitad(entrada, salida);
 
         if (guardarSiguienteImagen) {//Para foto salida debe ser rgba
             takePhoto(entrada, salida);
