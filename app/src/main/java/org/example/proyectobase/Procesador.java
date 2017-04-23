2f39ace53480a95cd6d284556539c65f62567ebf
José Alberto Martín <jalbertomartinfalcon@gmail.com>
2017-04-23 20:59:26 +0200

    first commit

--- app/src/main/java/org/example/proyectobase/Procesador.java
+++ app/src/main/java/org/example/proyectobase/Procesador.java
@@ -20,12 +20,22 @@ import java.util.List;
 public class Procesador {
 
     Procesador() {
-
     }
 
     public Mat procesa(Mat entrada) {
         Mat salida = new Mat();
         Imgproc.equalizeHist(entrada, salida);
         return salida;
+    }
+
+    void mitadMitad(Mat entrada, Mat salida) {
+//Representar la entrada en la mitad izquierda
+        Rect mitad_izquierda = new Rect();
+        mitad_izquierda.x = 0; mitad_izquierda.y = 0;
+        mitad_izquierda.height = entrada.height();
+        mitad_izquierda.width = entrada.width()/2;
+        Mat salida_mitad_izquierda = salida.submat( mitad_izquierda );
+        Mat entrada_mitad_izquierda = entrada.submat( mitad_izquierda );
+        entrada_mitad_izquierda.copyTo(salida_mitad_izquierda);
     }
 }
