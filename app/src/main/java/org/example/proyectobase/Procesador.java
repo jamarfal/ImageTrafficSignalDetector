215d6e165154f25ed1c193f086062bd1c36b114f
José Alberto Martín <jalbertomartinfalcon@gmail.com>
2017-04-24 21:56:24 +0200

    Achieved

--- app/src/main/java/org/example/proyectobase/Procesador.java
+++ app/src/main/java/org/example/proyectobase/Procesador.java
@@ -12,11 +12,13 @@ public class Procesador {
     Mat green;
     Mat blue;
     Mat maxGB;
+    Mat maxRB;
     public Procesador() { //Constructor
         red = new Mat();
         green = new Mat();
         blue = new Mat();
         maxGB = new Mat();
+        maxRB = new Mat();
     }
     public Mat procesa(Mat entrada) {
         Mat salida = new Mat();
@@ -27,4 +29,16 @@ public class Procesador {
         Core.subtract( red , maxGB , salida );
         return salida;
     }
+
+    public Mat procesaGreen(Mat entrada) {
+        Mat salida = new Mat();
+        Core.extractChannel(entrada, red, 0);
+        Core.extractChannel(entrada, green, 1);
+        Core.extractChannel(entrada, blue, 2);
+        Core.max(red, blue, maxRB);
+        Core.subtract( green , maxRB , salida );
+        return salida;
+    }
+
+
 }
