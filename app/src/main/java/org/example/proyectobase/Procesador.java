9533cb21adc04a91b6bfa162706f5cb9858144a5
José Alberto Martín <jalbertomartinfalcon@gmail.com>
2017-05-01 20:25:11 +0200

    firs commit

--- app/src/main/java/org/example/proyectobase/Procesador.java
+++ app/src/main/java/org/example/proyectobase/Procesador.java
@@ -2,6 +2,7 @@ package org.example.proyectobase;
 
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
 
 /**
  * Created by jamarfal on 19/4/17.
@@ -27,18 +28,22 @@ public class Procesador {
         Core.extractChannel(entrada, blue, 2);
         Core.max(green, blue, maxGB);
         Core.subtract( red , maxGB , salida );
+        Core.MinMaxLocResult minMax = Core.minMaxLoc(salida);
+        int maximum = (int) minMax.maxVal;
+        int thresh = maximum / 4;
+        Imgproc.threshold(salida, salida, thresh, 255, Imgproc.THRESH_BINARY);
         return salida;
     }
 
-    public Mat procesaGreen(Mat entrada) {
-        Mat salida = new Mat();
-        Core.extractChannel(entrada, red, 0);
-        Core.extractChannel(entrada, green, 1);
-        Core.extractChannel(entrada, blue, 2);
-        Core.max(red, blue, maxRB);
-        Core.subtract( green , maxRB , salida );
-        return salida;
-    }
+//    public Mat procesaGreen(Mat entrada) {
+//        Mat salida = new Mat();
+//        Core.extractChannel(entrada, red, 0);
+//        Core.extractChannel(entrada, green, 1);
+//        Core.extractChannel(entrada, blue, 2);
+//        Core.max(red, blue, maxRB);
+//        Core.subtract( green , maxRB , salida );
+//        return salida;
+//    }
 
 
 }
