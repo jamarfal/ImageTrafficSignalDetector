799c6ad2ce875bcd6409fdef72f2139b2332d196
José Alberto Martín <jalbertomartinfalcon@gmail.com>
2017-05-09 21:04:08 +0200

    first commit

--- app/src/main/java/org/example/proyectobase/Procesador.java
+++ app/src/main/java/org/example/proyectobase/Procesador.java
@@ -6,6 +6,7 @@ import org.opencv.core.MatOfFloat;
 import org.opencv.core.MatOfInt;
 import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
+import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
 
 import java.util.ArrayList;
@@ -16,10 +17,22 @@ import java.util.List;
  */
 
 public class Procesador {
+    Mat paso_bajo;
+
     public Procesador() { //Constructor
+        paso_bajo = new Mat();
     }
+
     public Mat procesa(Mat entrada) {
-        Mat salida = entrada.clone();
+        Mat salida = new Mat();
+        int filter_size = 17;
+        Size s = new Size(filter_size, filter_size);
+        Imgproc.blur(entrada, paso_bajo, s);
+// Hacer la resta. Los valores negativos saturan a cero
+        Core.subtract(paso_bajo, entrada, salida);
+//Aplicar Ganancia para ver mejor. La multiplicacion satura
+        Scalar ganancia = new Scalar(2);
+        Core.multiply(salida, ganancia, salida);
         return salida;
     }
 }
