7b29707d031379a3ef110937c575915a953a01ee
José Alberto Martín <jalbertomartinfalcon@gmail.com>
2017-05-09 16:30:18 +0200

    First commit

--- app/src/main/java/org/example/proyectobase/Procesador.java
+++ app/src/main/java/org/example/proyectobase/Procesador.java
@@ -4,6 +4,8 @@ import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfFloat;
 import org.opencv.core.MatOfInt;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.Point;
 import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
 import org.opencv.core.Size;
@@ -17,26 +19,61 @@ import java.util.List;
  */
 
 public class Procesador {
-    public Procesador() { //Constructor
+
+    double tam =11;
+    Mat gray_dilation;
+    Mat dilation_residue;
+    int contraste = 11;
+    int tamano = 7;
+
+    public Procesador() {
+        gray_dilation = new Mat();
+        dilation_residue = new Mat();
     }
-
     public Mat procesa(Mat entrada) {
-        double tam = 11;
+        Mat binaria = new Mat();
+        Mat SE = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(tam,tam));
+        Imgproc.dilate(entrada, gray_dilation, SE );
 
-        // Elemento estructurante
-        Mat SE = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(tam, tam));
-        // Dilatación
-        Mat gray_dilation = new Mat(); // Result
-        Imgproc.dilate(entrada, gray_dilation, SE); // 3x3 dilation
-        // Cálculo del residuo
-        Mat dilation_residue = new Mat();
         Core.subtract(gray_dilation, entrada, dilation_residue);
 
-        //Calculo del gradiente morfológico.
-        int contraste = 2;
-        int tamano = 7;
-        Imgproc.adaptiveThreshold(gray_dilation, dilation_residue,255, Imgproc.ADAPTIVE_THRESH_MEAN_C,
+        Imgproc.adaptiveThreshold(dilation_residue, binaria, 255, Imgproc.ADAPTIVE_THRESH_MEAN_C,
                 Imgproc.THRESH_BINARY, tamano, -contraste );
-        return dilation_residue;
+
+        List<MatOfPoint> blobs = new ArrayList< MatOfPoint > () ;
+        Mat hierarchy = new Mat();
+        Mat salida = binaria.clone();//Copia porque finContours modifica entrada
+        Imgproc.cvtColor(salida, salida, Imgproc.COLOR_GRAY2RGBA);
+        Imgproc.findContours(binaria, blobs, hierarchy, Imgproc.RETR_CCOMP,
+                Imgproc.CHAIN_APPROX_NONE );
+        int minimumHeight = 30;
+        float maxratio = (float) 0.75;
+// Seleccionar candidatos a circulos
+        for (int c= 0; c< blobs.size(); c++ ) {
+            double[] data = hierarchy.get(0,c);
+            int parent = (int) data[3];
+            if(parent < 0) //Contorno exterior: rechazar
+                continue;
+            Rect BB = Imgproc.boundingRect(blobs.get(c) );
+// Comprobar tamaño
+            if ( BB.width < minimumHeight || BB.height < minimumHeight)
+                continue;
+// Comprobar anchura similar a altura
+            float wf = BB.width;
+            float hf = BB.height;
+            float ratio = wf / hf;
+            if(ratio < maxratio || ratio > 1.0/maxratio)
+                continue;
+// Comprobar no está cerca del borde
+            if(BB.x < 2 || BB.y < 2)
+                continue;
+            if(entrada.width() - (BB.x + BB.width) < 3 || entrada.height() - (BB.y + BB.height) < 3)
+                continue;
+// Aqui cumple todos los criterios. Dibujamos
+            final Point P1 = new Point(BB.x, BB.y);
+            final Point P2 = new Point(BB.x+BB.width, BB.y+BB.height);
+            Imgproc.rectangle(salida, P1, P2, new Scalar(255,0,0) );
+        } // for
+        return salida;
     }
 }
